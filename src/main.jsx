c86f33e3692c0ac7447b976c21ded3bd2805fdb9
prabheen09876 <prabheen09876@gmail.com>
2024-01-07 22:20:26 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+// main.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Change 'Switch' to 'Routes'
+import HomePage from './Components/HomePage';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const Main = () => {
+  return (
+    <Router>
+      <Routes>  {/* Change 'Switch' to 'Routes' */}
+        <Route path="/" element={<HomePage />} />
+        {/* Add more routes for other pages */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default Main;
